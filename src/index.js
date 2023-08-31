924c4ce235f0e315084570af3eea0519e58b615d
Boris Vorobev <105520994+borisvorobev@users.noreply.github.com>
2023-08-30 20:10:21 -0400

    add modal container for react native

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as ModalContainer } from './ModalContainer'
+export { default as ModalContainerNative } from './ModalContainer.native'
 export { default as Reoverlay } from './Reoverlay'
 export { default as ModalWrapper } from './ModalWrapper'
